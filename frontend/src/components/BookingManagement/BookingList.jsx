e409e26887807024e9fb6b6622431ce05491758b
AyobamiH <heryourbarme@live.com>
2024-05-11 12:13:41 +0100

    Margin top for BookingList

--- frontend/src/components/BookingManagement/BookingList.jsx
+++ frontend/src/components/BookingManagement/BookingList.jsx
@@ -86,7 +86,7 @@ const BookingList = () => {
   if (error) return <div>{error}</div>;
 
   return (
-    <div className="overflow-x-auto mt-12">
+    <div className="overflow-x-auto mt-24">
       <table className="min-w-full divide-y divide-gray-200">
         <thead className="bg-gray-50">
           {/* Table Headers */}
