a0087afade6ed63cf5c152dea9a6fc0294a0ba81
AyobamiH <heryourbarme@live.com>
2024-08-31 15:00:21 +0100

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,48 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     historyApiFallback: true, // Ensure client-side routing works
-//     proxy: {
-//       '/api': {
-//         target: 'https://pamelas-pampered-pets-website-1.onrender.com',  // Your backend server
-//         changeOrigin: true,
-//         secure: false,
-//       },
-    
-//     },
-//     build: {
-//     rollupOptions: {
-//       external: ['passport', 'express-session'] // Exclude these from the client bundle
-//     }
-//   },
-//     port: 5174,
-    
-//   },
-  
-
-// });
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
 });
 
 
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     proxy: {
-//       '/api': 'https://backend-c469.onrender.com/', // Adjust this to match your backend URL
-//     },
-//   },
-// });
